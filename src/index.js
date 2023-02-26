4d484121a7197a4c384bd409b0e585aae0a6d56e
skryer <stealth.pro@gmail.com>
2023-02-26 19:56:41 +0300

    i done brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const obj= {};
+  let stack = [];
+
+  for(let i=0; i<bracketsConfig.length; i++) {
+    obj[bracketsConfig[i][0]]=bracketsConfig[i][1];
+  }
+    for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+
+      
+      if (obj[currentSymbol]) {
+        if(obj[currentSymbol]===currentSymbol && stack[stack.length-1]===currentSymbol) stack.pop();
+        else stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+
+        let topElement = stack[stack.length - 1];
+
+        if (obj[topElement] === currentSymbol) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0;
 }
